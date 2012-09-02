20c8477f727cdceccd8b247e57032fc5631e8508
JinBlack <jinblack17@gmail.com>
2012-09-02 16:50:21 +0200

    P17 Solved

--- P17.scm
+++ P17.scm
@@ -1,7 +1,11 @@
 #lang scheme
 (define (split l n)
-  (if (equal? n 1)
-      n
-      (cons (cons (car l) (car (split (cdr l) (- n1)))) (cdr (split (cdr l) (- n1))))
+  (if (equal? n 0)
+      (cons '() (cons l '()))
+      (cons (cons (car l) 
+                  (car (split (cdr l) (- n 1)))) (cdr (split (cdr l) (- n 1))))
       )
+  )
+
 ;;test
+(split '(a b c d e f g h i) 3)
